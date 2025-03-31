193773939dbe83f76fed54aa7db145b86e88607f
AKULA SRI HARSHA SRI SAI HANUMAN <116995673+AHarshaNaidu@users.noreply.github.com>
2025-03-31 20:58:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,7 +124,7 @@ You are an AI assistant specialized in generating Wireframes and Mockups. Use th
 
 def call_llm_api(prompt_template, user_content):
     data = {
-        "model": "llama-3.2-3b-preview",
+        "model": "llama-3.2-1b-preview",
         "messages": [
             {"role": "system", "content": prompt_template},
             {"role": "user", "content": user_content}
